502b839b54b4a0f1bacd1726dfed8f791ba960bf
Android-Penguin <49669409+Android-Penguin@users.noreply.github.com>
2024-06-04 02:03:20 +1200

    Breakpoints - Added CSS breakpoints to prevent site completely cooking itself on mobile. - Added JS script for possible future redirect.

---
+++ mobile.js
@@ -0,0 +1,8 @@
+window.addEventListener('resize', function () {
+    var windowWidth = window.innerWidth;
+    // console.log('Window width: ' + windowWidth + 'px');
+
+    if (windowWidth < 930) {
+        console.log("AHHHHHH")
+    }
+});
